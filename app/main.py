6ad6cfee7f38d208727bebea1a3e21ad76746139
Ilya Charauko <75255156+MirrSs@users.noreply.github.com>
2022-08-14 14:58:05 +0300

    major functionality update,playlist bugs discovered

--- app/main.py
+++ app/main.py
@@ -42,7 +42,10 @@ def get_current_time(song_length):
     
     global current_song
     if song_pos_slider.get() == int(MP3(current_song).info.length):
-        return
+        if playlist[-1] == current_song:
+            next_song(next=1)
+        else:
+            next_song()
     after_id = slider_label.after(1000,get_current_time,song_length)
 
 
@@ -75,19 +78,26 @@ def play_song():
 global paused
 paused = False
 def pause_song(is_paused):
+    global after_id
     global paused
     paused = is_paused
 
     if paused:
+        after_id = slider_label.after(1000,get_current_time,MP3(current_song).info.length)
         pygame.mixer.music.unpause()
         paused = False
     else:
+        slider_label.after_cancel(after_id)
         pygame.mixer.music.pause()
         paused = True
 
 def stop_song():
-     pygame.mixer.music.stop()
-     playlist_box.select_clear(ACTIVE)
+    global after_id
+    slider_label.after_cancel(after_id)
+    pygame.mixer.music.stop()
+    song_pos_slider.config(value=0)
+    slider_label.config(text='00:00:00')
+    playlist_box.select_clear(ACTIVE)
 
 def delete_song_from_playlist():
     song = playlist_box.selection_get()
@@ -97,31 +107,68 @@ def delete_song_from_playlist():
     song = playlist_box.curselection()
     playlist_box.delete(song)
 
-def next_song():
-    current_song = playlist_box.curselection()
-    song = playlist_box.get(current_song[0]+1)
+def next_song(next=None):
+    global after_id
+    global current_song
+    slider_label.after_cancel(after_id)
+    song_pos_slider.config(value=0)
+    slider_label.config(text='00:00:00')
+    if(next==None):
+        curr_song = playlist_box.curselection()
+        if playlist[-1] == current_song:
+            song = playlist_box.get(0)
+        else:
+            song = playlist_box.get(curr_song[0]+1)
+    else:
+        song = playlist_box.get(0)
+        curr_song = playlist_box.curselection()
     for file in playlist:
         if file.find(song)>=0:
+            song_len = MP3(file).info.length
+            current_song = file
             pygame.mixer.music.load(file)
             pygame.mixer.music.play(loops=0)
+            get_current_time(song_len)
+            slider_position = int(song_len)
+    song_pos_slider.config(to=slider_position,value=0)
     
     #move active bar in playlist
     playlist_box.selection_clear(0,END)
-    playlist_box.activate(current_song[0]+1)
-    playlist_box.selection_set(current_song[0]+1,last=None)
+    # curr_song[0]+1
+    #logger.debug(curr_song)
+    if playlist[-1] == current_song:
+        playlist_box.activate(0)
+        playlist_box.selection_set(0,last=None)
+    else:
+        playlist_box.activate(curr_song[0]+1)
+        playlist_box.selection_set(curr_song[0]+1,last=None)
 
 def prev_song():
-    current_song = playlist_box.curselection()
-    song = playlist_box.get(current_song[0]-1)
+    global after_id
+    slider_label.after_cancel(after_id)
+    slider_label.config(text='00:00:00')
+    curr_song = playlist_box.curselection()
+    song = playlist_box.get(curr_song[0]-1)
     for file in playlist:
         if file.find(song)>=0:
+            song_len = MP3(file).info.length
+            global current_song
+            current_song = file
             pygame.mixer.music.load(file)
             pygame.mixer.music.play(loops=0)
+            get_current_time(song_len)
+            slider_position = int(song_len)
+    song_pos_slider.config(to=slider_position,value=0)
     
     #move active bar in playlist
     playlist_box.selection_clear(0,END)
-    playlist_box.activate(current_song[0]-1)
-    playlist_box.selection_set(current_song[0]-1,last=None)
+    playlist_box.activate(curr_song[0]-1)
+    playlist_box.selection_set(curr_song[0]-1,last=None)
+
+def volume_change(x):
+    pygame.mixer.music.set_volume(volume_slider.get())
+    volume_frame.config(text=f'Volume\n{int(volume_slider.get()*100)}%')
+    volume_frame.after(200,volume_change,x)
 
 def song_slider(arg):
     global current_song
@@ -144,15 +191,24 @@ window.geometry('600x400')
 #mixer init
 pygame.mixer.init()
 
-#Create playlist box
-playlist_box = Listbox(window,bg='black',fg='white',width=60)
-playlist_box.pack(pady=20)
+#Master frame for playlist and volume control
+master_frame = Frame(window)
+master_frame.pack(pady=20)
 
+volume_frame = LabelFrame(master_frame,text=f'Volume\n100%')
+volume_frame.grid(row=0,column=1,padx=20)
+
+#Create playlist box
+playlist_box = Listbox(master_frame,bg='black',fg='white',width=60)
+playlist_box.grid(row=0,column=0)
+
+volume_slider = ttk.Scale(volume_frame,from_=1,to=0,orient=VERTICAL,value=1,command=volume_change,length=120)
+volume_slider.pack(pady=21)
 # status_bar = Label(window,text='',bd=1,relief=GROOVE,anchor=E)
 # status_bar.pack(fill=X,side=BOTTOM,ipady=2)
 
-controls_frame = Frame(window)
-controls_frame.pack()
+controls_frame = Frame(master_frame)
+controls_frame.grid(row=1,column=0,pady=20)
 
 #Button images
 backward_btn_img = PhotoImage(file='images/backward.png')
@@ -179,11 +235,11 @@ stop_btn.grid(row=0,column=4,padx=7)
 add_btn.grid(row=0,column=5,padx=7)
 delete_btn.grid(row=0,column=6,padx=7)
 
-duration_frame = Frame(window)
-duration_frame.pack()
+duration_frame = Frame(master_frame)
+duration_frame.grid(row=3,column=0,pady=20)
 
 song_pos_slider = ttk.Scale(duration_frame,from_=0,to=100,orient=HORIZONTAL,value=0,command=song_slider,length=350)
-slider_label = Label(duration_frame,text='0')
+slider_label = Label(duration_frame,text='00:00:00')
 duration_label = Label(duration_frame,text='')
 
 slider_label.grid(row=0,column=0,padx=4,pady=5)
